f7e9af9f138637ebf6e20e065da444d6bd1e8fb2
DaveCorleto <davide.corleto@fastwebnet.it>
2024-01-25 17:27:38 +0100

    scaffolding init

---
+++ src/array.php
@@ -0,0 +1,16 @@
+<?php
+
+    $todolist = [
+        ['Fare la spesa', false],
+        ['Studiare PHP', false],
+        ['Allenarsi in palestra', false],
+        ['Leggere un libro', false],
+        ['Guardare un film', false],
+        ['Scrivere codice', false],
+        ['Preparare la cena', false],
+        ['Fare una passeggiata', false],
+        ['Ascoltare musica', false],
+        ['Imparare una nuova lingua', false]
+    ];
+
+?>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,24 @@
+<script>
+import { ref } from 'vue';
+import axios from 'axios';
+export default {
+  name: "AppMain", 
+}
+
+
+</script>
+
+<template>
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+</template>
+
+<style scoped>
+
+</style>
